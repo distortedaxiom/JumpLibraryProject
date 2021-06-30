de1f0057969dd6fc443392b584395dad52e06541
Disto <48456332+DistortedAxiom@users.noreply.github.com>
2021-06-30 11:44:26 -0400

    changed connection

--- src/main/java/com/cognixia/jump/connection/ConnectionManager.java
+++ src/main/java/com/cognixia/jump/connection/ConnectionManager.java
@@ -8,7 +8,7 @@ public class ConnectionManager {
 
     private static final String URL = "jdbc:mysql://localhost:3306/library";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Voidcrash97";
+    private static final String PASSWORD = "Password";
 
     private static Connection connection = null;
 
